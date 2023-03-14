476456ffe5a81c6dfcff87a4a3c2de5bb48c0bd5
Velocichicken <velocichicken1@gmail.com>
2023-03-13 21:40:44 -0500

    Update AirFramework..py

--- src/AirFramework..py
+++ src/AirFramework..py
@@ -1,5 +1,22 @@
 class aircraft:
 
     # class constructor
-    def __init__(self, name, catagory, airframeRigidity, stallAOA, stallTAS, maxTAS, vsM, turnRate, flaps, acceleration, fuelCapacity, maxAltitude, fuelburnRate):
-      
+    def __init__(self, name, catagory, airframeRigidity, stallAOA, stallTAS, maxTAS, vsM, turnRate, flaps, acceleration, fuelCapacity, maxAltitude, fuelburnRate, maxFlaps):
+      self.name = name
+      self.catagory = catagory
+      self.airframeRigidity = airframeRigidity
+      self.stallAOA = stallAOA
+      self.stallTAS = stallTAS
+      self.maxTAS = maxTAS
+      self.vsM = vsM
+      self.turnRate = turnRate
+      self.flaps = flaps
+      self.acceleration = acceleration
+      self.fuelCapacity = fuelCapacity
+      self.maxAltitude = maxAltitude
+      self.fuelBurnRate = fuelburnRate
+      self.maxFlaps = maxFlaps
+
+
+class airport:
+   pass
