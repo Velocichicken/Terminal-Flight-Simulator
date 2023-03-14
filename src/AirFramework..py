493d279f429123bacd098b0b08127faac1e84103
Velocichicken <velocichicken1@gmail.com>
2023-03-13 21:30:57 -0500

    A addition of all the files needed

---
+++ src/AirFramework..py
@@ -0,0 +1,5 @@
+class aircraft:
+
+    # class constructor
+    def __init__(self, name, catagory, airframeRigidity, stallAOA, stallTAS, maxTAS, vsM, turnRate, flaps, acceleration, fuelCapacity, maxAltitude, fuelburnRate):
+      
